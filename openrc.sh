f5c2381145379c72de39dbf6abd32808fa73392f
jja6312 <127708710+jja6312@users.noreply.github.com>
2025-07-27 23:22:21 +0900

    init

---
+++ openrc.sh
@@ -0,0 +1,9 @@
+export OS_AUTH_URL=http://133.186.203.38/identity
+export OS_PROJECT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=2025_osc_openstack
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_DOMAIN_ID=default
+export OS_IDENTITY_API_VERSION=3
+export OS_AUTH_TYPE=password
+
